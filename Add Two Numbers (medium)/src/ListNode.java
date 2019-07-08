11453d7937f4501d6fd6101e2c0cc9ae93af8004
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-07-07 21:44:58 -0500

    AddingTwoNumbers LinkedList leetcode problem

--- Add Two Numbers (medium)/src/ListNode.java
+++ Add Two Numbers (medium)/src/ListNode.java
@@ -1,43 +1,12 @@
-public class ListNode {
-	private int data;
-	private ListNode link;
-	 
-//public ListNode(int data) { 
-	// ListNode head;
-	 //head = new ListNode(data, head);
-	//}
-
-public ListNode(int initialData, ListNode initialLink) {
-	this.data = initialData;
-	this.link = initialLink;
-}
-
-public void addNode(int data,ListNode head) {
-	head = new ListNode(data,head);
-}
-
-public int getData() {
-	return data;
-}
-
-public ListNode getLink() {
-	return link;
-}
-
-public void setData(int data) {
-	this.data = data;
-}
-
-public void setLink(ListNode newLink) {
-	this.link = newLink;
-}
-
-//public void addNode(ListNode head) {
-	//head = new ListNode(data, head);
-//}
-
-public void addNodeAfter(int element) {
-	link = new ListNode(element, link);
-}
-
-}
+// Definition for singly-linked list.
+  public class ListNode {
+	  
+      int val;
+      ListNode next;
+      
+     ListNode(int x) { 
+         val = x;
+         }
+         
+  }
+ 
--- Add Two Numbers (medium)/src/Solution.java
+++ Add Two Numbers (medium)/src/Solution.java
@@ -1,86 +1,42 @@
-
-class Solution {
-	
-   // public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        //return ListNode;   
-   // }
-    
-    public static void main(String[] args) {
-    	// 2 -> 4 -> 3
-    	// 5 -> 6 -> 4
-    	
-    	ListNode l1 = new ListNode(2,null);
-    	ListNode l2 = new ListNode(5,null);
-    	ListNode l3 = new ListNode(69,null);
-    	
-    	// Adding to the head of the Linked List
-    	ListNode test1 = null;
-    	test1 = new ListNode(25,test1);
-    	test1 = new ListNode(33,test1);
-    	test1 = new ListNode(45,test1);
-    	
-    	
-    	//ListNode testChain = new ListNode(33,(55, null) , (77, null)
-    	
-
-    	l3.addNode(45,l3);
-    	
-    	ListNode selection;
-    	selection = l1;
-    	l1.addNodeAfter(4);
-     	
-    	selection = l1.getLink();
-    	selection.addNodeAfter(3);
-    	
-    	ListNode selection2;
-    	selection2 = l2;
-    	l2.addNodeAfter(6);
-    	
-    	selection2 = l2.getLink();
-    	l2.addNodeAfter(4);
-    	
-    	
-    	System.out.println("l1:");
-    	ListNode cursor;
-    	for(cursor = l1; cursor != null ; cursor = cursor.getLink()) {
-    		System.out.print(cursor.getData() + " -> ");
-    	}
-    	System.out.println();
-    	System.out.println();
-    	
-    	System.out.println("l2:");
-    	ListNode cursor2;
-    	for(cursor2 = l2; cursor2 != null ; cursor2 = cursor2.getLink()) {
-    		System.out.print(cursor2.getData() + " -> ");
-    	}
-    	
-    	System.out.println();
-    	System.out.println();
-    	
-    	System.out.println("l3:");
-    	ListNode cursor3;
-    	for(cursor3 = l3; cursor3 != null ; cursor3 = cursor3.getLink()) {
-    		System.out.print(cursor3.getData() + " -> ");
-    	}
-    	
-    	System.out.println();
-    	System.out.println();
-    	
-    	System.out.println("test1:");
-    	ListNode cursor4;
-    	for(cursor4 = test1; cursor4 != null ; cursor4 = cursor4.getLink()) {
-    		System.out.print(cursor4.getData() + " -> ");
-    	}
-    	
-    	
-    	System.out.println("Adding:");
-    	
-    	ListNode cursorAdd1;
-    	ListNode cursorAdd2 = l2;
-    	for(cursorAdd1 = l1; cursorAdd1 != null ; cursorAdd1 = cursorAdd1.getLink()) {
-    		
-    		System.out.print(cursorAdd1.getData() + cursorAdd2.getData());
-    		cursorAdd2 = cursorAdd2.getLink();
-    		}
-    }
-}
+public class Solution {
+	 public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+	        ListNode dummyHead = new ListNode(0);
+	        ListNode curr = dummyHead;
+	        int carry = 0;
+	        int sum = 0;
+	        int x = 0;
+	        int y = 0;
+	       
+	        ListNode q = l2;      
+	        for(ListNode p = l1; p != null; p = p.next) {
+	           if (p != null || q != null) 
+	           {
+	               x = p.val;
+	                y = q.val;
+	           }
+	            else if (p == null || q == null)
+	            {
+	                 x = 0;
+	                 y = 0;
+	            }
+	            
+	            sum = x + y + carry;
+	            
+	            carry = sum/10; // sets carry to 1 or 0
+	            curr.next = new ListNode(sum % 10);
+	                curr = curr.next;
+	            if(q != null) {
+	                 q = q.next;
+	            }
+	        }
+	        
+	       if (carry > 0 ) {
+	          curr.next = new ListNode(carry);
+	       }
+	        
+	        return dummyHead.next;
+	    }
+	 public static void main (String[] args) {
+		 System.out.println("Hello World");
+	 }
+	}

8420fbc4c326939220faa423eda9db1110f0209b
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-08-07 01:39:19 -0500

    updated conditional flow of longest substring

--- LongestSubstringWithoutRepeatingChars/src/Solution.java
+++ LongestSubstringWithoutRepeatingChars/src/Solution.java
@@ -1,56 +1,64 @@
-
 public class Solution {
-	  public void lengthOfLongestSubstring(String s) {
+	  public int lengthOfLongestSubstring(String s) {
 		 int currCounter = 0;
 	     int memCounter = 0; 
 	     String currString = "";
 	     String memString = "";     
 	     int i = 0;
 	     int j = i+1;
-     
+	     
+	     if (s.length() == 0) {
+	            return 0;
+	        }
+	        else if(s.length() == 1) {
+	            return 1;
+	        }
+	        else {	                 
 	   while (i < s.length() -1) {
 		   currString = Character.toString(s.charAt(i));	
-		   currCounter++;
-	   while(j < s.length()-1) {
-		   currString = currString + s.charAt(j);
-		   currCounter++;
-		   j++;
-		   if (currString.substring(0,currCounter-1).contains(Character.toString(s.charAt(j)))) 
-		   {
+		  currCounter++;
+	   while(j <= s.length()-1) {
+		   currString = currString + s.charAt(j);		 
+		   currCounter++;		  	
+		   if (currString.substring(0,currCounter-1).contains(Character.toString(s.charAt(j))) && currCounter > memCounter) 
+		   {			 
 			   memString = currString.substring(0,currString.length()-1);
 			   memCounter = currCounter-1;
-			   System.out.println("string memory is: " + memString + " when i= "+ i + " and j = " + j);
-			   System.out.println("They are equal at i=" + i + " and j= " + j);
-			   System.out.println("Counter value is: " + memCounter);
-			   
+			   //System.out.println("string memory is: " + memString + " when i= "+ i + " and j = " + j);
+			  //System.out.println("They are equal at i=" + i + " and j= " + j);
+			  //System.out.println("Counter value is: " + memCounter);		   
 			   currString = "";
 			   i = j-1;
 			   j++;
-			   System.out.println();
-			  
-			   
+			   //System.out.println();						   
 			   currCounter = 0;
 			   break;		   
 		   }
 		   j++;
 	   }
+	   
 	   i++; 
-	  }	   
-	   System.out.println("Longest substring word is: " + memString);
-	   System.out.println("Longest substring length is: " + memCounter);
-	   System.out.println("------------------------------------------");
+	   }
+	  }
+	     return memCounter;
+	   //System.out.println("Longest substring word is: " + memString);
+	   //System.out.println("Longest substring length is: " + memCounter);
+	   //System.out.println("------------------------------------------");
+	     
 	  }
 	  
 	  public static void main(String[] args) {
 		  String word1 =  "abcabcbb";
 		  String word2  = "bbbbb";
 		  String word3 = "pwwkew";
+		  String word4 = "au";
 		  
 		  Solution func = new Solution(); // instantiate object to run method of class
 		 
-		// func.lengthOfLongestSubstring(word1);
-		 // func.lengthOfLongestSubstring(word2);
-		 func.lengthOfLongestSubstring(word3);
+		System.out.println("The length of longest substring is: " + func.lengthOfLongestSubstring(word1));
+		System.out.println("The length of longest substring is: " + func.lengthOfLongestSubstring(word2));
+		System.out.println("The length of longest substring is: " + func.lengthOfLongestSubstring(word3));
+		System.out.println("The length of longest substring is: " + func.lengthOfLongestSubstring(word4));
 
 }
 }

da308443871dfedb90a833ed408a3f23b7576676
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-08-07 01:44:07 -0500

    adding TwoSum completed via brute force method ;)

---
+++ TwoSumBruteForce/src/TwoSum.java
@@ -0,0 +1,33 @@
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        int num1 = 0;
+        int num2 = 0;
+
+        for (int p1 = 0; p1 < nums.length; p1++) {
+            for (int p2 = 0; p2 < nums.length; p2++) {
+                if ((nums[p1] + nums[p2] == target) && (p1 != p2)) {
+                    num1 = p1;
+                    num2 = p2;
+                }
+            }
+        }
+        int[] retIndices = new int[2];
+        retIndices[1] = num1;
+        retIndices[0] = num2;
+        return retIndices;
+    }
+
+    public static void main(String args[]) {
+        int[] test = {2, 7 , 5 ,11};
+        TwoSum hey = new TwoSum();
+
+        int [] ans = hey.twoSum(test,9);
+
+        for(int i=0; i<ans.length; i++) {
+            System.out.println("The indices that add up to target are: " + ans[i] );
+        }
+    }
+}
+

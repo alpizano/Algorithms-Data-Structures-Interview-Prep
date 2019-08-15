1c67809382b6650e9fcb09bd1148c904431824e5
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-08-14 23:51:04 -0500

    updated algorithm

--- LongestSubstringWithoutRepeatingChars/src/Solution.java
+++ LongestSubstringWithoutRepeatingChars/src/Solution.java
@@ -10,68 +10,83 @@ public class Solution {
 	      if (s.length() == 0) {
 	            return 0;
 	        }
-	        else if(s.length() == 1) {
+	      else if(s.length() == 1) {
 	            return 1;
 	        }
-	        else if(s.length() == 2) {
+	      else if(s.length() == 2) {
 	            if(s.charAt(0) == s.charAt(1)) {
-	                return 1;
+	            	//System.out.println("The letters are equal");
+					memString = s.substring(0,s.length()-1);
+					memCounter = 1;               
 	            }
 	            else {
-	                return 2;
+	            	memString = s.substring(0,s.length());
+					memCounter = 2;  
 	            }
 	        }
-	        else {
-	                
+	      else {	                
 		   while (i < s.length() -1) {
 			   currString = Character.toString(s.charAt(i));	
 			  currCounter++;
 		   while(j <= s.length()-1) {
 			   currString = currString + s.charAt(j);		 
-			   currCounter++;		  	
-			   if (currString.substring(0,currCounter-1).contains(Character.toString(s.charAt(j))) && currCounter > memCounter) 
+			   currCounter++; // assume to be equal
+			   // LCS breaks but only update if bigger than current LCS length
+ 			   if (currString.substring(0,currString.length()-1).contains(Character.toString(s.charAt(j))) && currCounter >= memCounter) 
 			   {			 
 				   memString = currString.substring(0,currString.length()-1);
-				   memCounter = currCounter-1;			  	   
+				  memCounter = currCounter-1;			  	   
 				   currString = "";
-				   i = j-1;
-				   j++;			  					   
+				   i = j-2; // when LCS cuts off, start i at where j was at
+				   //j++;			  					   
 				   currCounter = 0;
-				   break;		   
+				  break;		   
 			   }
-			   if(currCounter > memCounter) {
-				   memString = currString;
-				   memCounter = currCounter;
-				   
+			   //(currCounter > memCounter) {
+ 			   // Found newest LCS but didn't break yet
+			   else if (currCounter >= memCounter) {
+				   memString = currString.substring(0,currString.length());
+				   memCounter = currCounter;				   
+			   }
+			   
+			   else {
+				  // currCounter++;
+				  // memString = currString;
+				  // memCounter = currCounter;				   		
 			   }
 			   j++;
 		   }
 		   i++; 
 		  }	   
 	        }
-	     return memCounter;
-	   //System.out.println("Longest substring word is: " + memString);
-	   //System.out.println("Longest substring length is: " + memCounter);
-	   //System.out.println("------------------------------------------");
-	     
+	      System.out.println("memstring is: " + memString);			   
+	     return memCounter;	   	     
 	  }
 	  
 	  public static void main(String[] args) {
-		  String word1 =  "abcabcbb";
-		  String word2  = "bbbbb";
-		  String word3 = "pwwkew";
-		  String word4 = "au";
-		  String word5 = "aab";
-		  String word6 = "dvdf";
+		  String word1 =  "abcabcbb"; // answer = 3
+		  String word2  = "bbbbb"; // answer = 1
+		  String word3 = "pwwkew"; // answer = 3
+		  String word4 = "aa"; // answer = 1
+		  String word5 = "au"; // answer = 2
+		  String word6 = "aab"; // answer = 2
+		  String word7 = "dvdf"; // answer = 3
 		  
 		  Solution func = new Solution(); // instantiate object to run method of class
 		 
-		//System.out.println("The length of longest substring is: " + func.lengthOfLongestSubstring(word1));
-		//System.out.println("The length of longest substring is: " + func.lengthOfLongestSubstring(word2));
-		//System.out.println("The length of longest substring is: " + func.lengthOfLongestSubstring(word3));
-		//System.out.println("The length of longest substring is: " + func.lengthOfLongestSubstring(word4));
-		 // System.out.println("The length of longest substring is: " + func.lengthOfLongestSubstring(word5));
-		  System.out.println("The length of longest substring is: " + func.lengthOfLongestSubstring(word6));
+	//	System.out.println("LCS of " + "\"" + word1 + "\" " + "is: " + func.lengthOfLongestSubstring(word1));
+	//	 System.out.println();			
+		// System.out.println("LCS of " + "\"" + word2 + "\"" + " is: " + func.lengthOfLongestSubstring(word2));
+		//  System.out.println();			
+		// System.out.println("LCS of " + "\"" + word3 + "\"" + " is: " + func.lengthOfLongestSubstring(word3));
+		//  System.out.println();		
+		//System.out.println("LCS of " + "\"" +  word4 + "\"" + " is: " + func.lengthOfLongestSubstring(word4));
+		//System.out.println();
+		//System.out.println("LCS of " + "\"" + word5 + "\"" + " is: " + func.lengthOfLongestSubstring(word5));
+		// System.out.println();		
+		 System.out.println("LCS of " + "\"" + word6 + "\"" + " is: " + func.lengthOfLongestSubstring(word6));
+		  System.out.println();	
+		 System.out.println("LCS of " + "\"" + word7 + "\"" + " is: " + func.lengthOfLongestSubstring(word7));
 
 }
 }

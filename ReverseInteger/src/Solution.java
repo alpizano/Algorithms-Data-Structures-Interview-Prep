e4f04708c84ff0010394643519740b5b420f9494
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-08-26 00:22:40 -0500

    beginning reverseinteger problem

--- ReverseInteger/src/Solution.java
+++ ReverseInteger/src/Solution.java
@@ -1,4 +1,10 @@
-
-public class Solution {
-
+class Solution {
+    public int reverse(int x) {
+        return 0;
+    }
+    
+    
+    public static void main(String[] args) {
+    	System.out.println("Hello world");
+    }
 }

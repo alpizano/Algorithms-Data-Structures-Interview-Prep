9ab8f68aec1a5af227b88d3daeb2dce526f6eb3a
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-08-27 03:27:25 -0500

    using stack to reverse integer

--- ReverseInteger/src/Solution.java
+++ ReverseInteger/src/Solution.java
@@ -1,10 +1,43 @@
+import java.util.Stack;
+
 class Solution {
     public int reverse(int x) {
-        return 0;
+    	
+    	String xStr = Integer.toString(x);
+    	int ans = 0;
+    	
+    	Stack<String> strStack = new Stack<>();
+    	Stack<Integer> numStack = new Stack<>();
+    	
+    	for(int i=0; i<xStr.length()-1; i++) {
+    		strStack.push(Character.toString(xStr.charAt(i)));
+    	}
+    	
+    	while(!strStack.isEmpty()) {
+    		xStr = xStr + strStack.pop();
+    	}
+    	
+    	System.out.println(xStr);
+    	ans = Integer.parseInt(xStr);
+    	
+    	return ans;
+    	
     }
     
     
     public static void main(String[] args) {
-    	System.out.println("Hello world");
+    	
+    	Solution solution = new Solution();
+    	int x = 321;
+    	
+    	solution.reverse(x);
+    	System.out.println(solution.reverse(x));
+    	
+    	
+    	
+    	
     }
+    
+    
+    
 }

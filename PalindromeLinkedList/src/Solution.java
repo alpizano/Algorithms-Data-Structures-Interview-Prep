7f1bffe041d9ded4a23b67af0e23bc541ccbd36c
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-08-29 14:32:54 -0500

    starting palindrome linked list leetcode problem

---
+++ PalindromeLinkedList/src/Solution.java
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

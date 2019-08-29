36d5645acd3ccdf56a07e764359c49e2feb36109
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-08-29 17:31:38 -0500

    started reverse linked list and fixed other src .java dirs

---
+++ ReverseLinkedList/src/Solution.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+	
+public ListNode reverseList(ListNode head) {
+	
+	return head;
+        
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

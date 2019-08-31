1ced4ff6c5f9d68a1052a6fa9225dd7a5af07309
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-08-31 03:56:10 -0500

    starting ValidParenthesis problem

---
+++ ValidParenthesis/src/Solution.java
@@ -0,0 +1,12 @@
+
+public class Solution {
+	   public boolean isValid(String s) {
+	        return true;
+	    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

ef623dcd629be42b807950f0b4c2c6a9ed915373
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-08-31 02:55:48 -0500

    finished reversing linked list iteratively

--- ReverseLinkedList/src/Solution.java
+++ ReverseLinkedList/src/Solution.java
@@ -5,28 +5,58 @@ import java.util.Stack;
  * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int x) { val = x; }
+ *     ListNode(int x) {
+ *     val = x; 
+ *     }
  * }
  */
 public class Solution {
 	
 public ListNode reverseList(ListNode head) {
 	
-	Stack<Integer> intStack = new Stack<>();
+	ListNode dummyList = null;
+	ListNode temp = null;
 	
-	for(ListNode cursor=head; cursor != null; cursor=cursor.next) {
-		intStack.push(cursor.val);
+	for(ListNode cursor=head; cursor != null; cursor=cursor.next) {		
+			temp = new ListNode(cursor.val);
+			temp.next = dummyList;
+			dummyList = temp;
+	
 	}
 	
 	
+	return dummyList;
 	
-	return head;
-	
-        
+     
     }
+
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		
+		Solution test = new Solution();
+		ListNode list1 = new ListNode(1);
+		list1.next = new ListNode(2);
+		list1.next.next = new ListNode(3);
+		list1.next.next.next = new ListNode(4);
+		list1.next.next.next.next = new ListNode(5);
+		
+		
+		//for(ListNode cursor=list1; cursor != null; cursor=cursor.next) { 
+			//System.out.print(cursor.val);
+		//}
+		
+
+		
+		ListNode dummyList = test.reverseList(list1);
+		
+		for(ListNode cursor= dummyList; cursor != null; cursor=cursor.next) { 
+			System.out.print(cursor.val);		
+		}
+		
 
 	}
 

007a0b46905e4a6f8d231752dad7768512c30f38
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-08-31 03:54:45 -0500

    add printout text for reverse list output

--- ReverseLinkedListIteratively/src/Solution.java
+++ ReverseLinkedListIteratively/src/Solution.java
@@ -9,52 +9,44 @@
  * }
  */
 public class Solution {
-	
-public ListNode reverseList(ListNode head) {
-	
-	ListNode dummyList = null;
-	ListNode temp = null;
-	
-	for(ListNode cursor=head; cursor != null; cursor=cursor.next) {		
+
+	public ListNode reverseList(ListNode head) {
+
+		ListNode dummyList = null;
+		ListNode temp = null;
+
+		for (ListNode cursor = head; cursor != null; cursor = cursor.next) {
 			temp = new ListNode(cursor.val);
 			temp.next = dummyList;
 			dummyList = temp;
-	
+
+		}
+
+		return dummyList;
+
 	}
-	
-	
-	return dummyList;
-	
-     
-    }
-
-
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
-		
+
 		Solution test = new Solution();
 		ListNode list1 = new ListNode(1);
 		list1.next = new ListNode(2);
 		list1.next.next = new ListNode(3);
 		list1.next.next.next = new ListNode(4);
 		list1.next.next.next.next = new ListNode(5);
-		
-		
-		//for(ListNode cursor=list1; cursor != null; cursor=cursor.next) { 
-			//System.out.print(cursor.val);
-		//}
-		
 
-		
-		ListNode dummyList = test.reverseList(list1);
-		
-		for(ListNode cursor= dummyList; cursor != null; cursor=cursor.next) { 
-			System.out.print(cursor.val);		
+		System.out.println("Original List:");
+		for(ListNode cursor=list1; cursor != null; cursor=cursor.next) {
+		System.out.print(cursor.val + " ");
 		}
 		
+		System.out.println();
+		System.out.println("Reversed List:");
+		ListNode dummyList = test.reverseList(list1);
+		for (ListNode cursor = dummyList; cursor != null; cursor = cursor.next) {
+			System.out.print(cursor.val + " ");
+		}
 
 	}
 

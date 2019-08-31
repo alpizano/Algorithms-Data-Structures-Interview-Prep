fc65e34701d37526ceb434a2c6f15bbf1d470964
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-08-31 16:56:04 -0500

    starting Merge Two Sorted Lists leetcode problem

---
+++ MergeTwoSortedLists/src/Solution.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+	 public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+		 ListNode mergedList = new ListNode(0);
+	        return mergedList;
+	    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

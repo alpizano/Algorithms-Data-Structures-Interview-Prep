e9b56815f76e1dd858d65bd9472168da1a4c4d5f
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-09-01 02:41:19 -0500

    finished merging linked list question

--- MergeTwoSortedLists/src/Solution.java
+++ MergeTwoSortedLists/src/Solution.java
@@ -50,27 +50,39 @@ public class Solution {
 		// }
 		// }
 
-		for (ListNode c1 = l1; c1 != null; c1 = c1.next) {
-			sNode.next = new ListNode(c1.val);
-			sNode = sNode.next;
-
-			if (c2 != null) {
-				if (c1.val < c2.val) {
+		ListNode c1 = l1;
+		//for (ListNode c1 = l1; c1 != null; c1 = c1.next) {
+		while(c1 != null) {
+			
+			while (c2 != null) {
+				if (c1 != null && c1.val < c2.val) {
 					sNode.next = new ListNode(c1.val);
-					sNode.next.next = new ListNode(c2.val);
-				} else if (c2.val < c1.val) {
+					sNode = sNode.next;
+					c1 = c1.next;				
+				} 
+				else if (c1 != null && c2.val < c1.val) {
 					sNode.next = new ListNode(c2.val);
-					sNode.next.next = new ListNode(c1.val);
-				} else {
+					sNode = sNode.next;
+					c2 = c2.next;					
+				} 	
+				else if(c1 != null && c1.val == c2.val) { 
 					sNode.next = new ListNode(c1.val);
 					sNode.next.next = new ListNode(c2.val);
+					sNode = sNode.next.next;
+					c1 = c1.next;
+					c2 = c2.next;				
 				}
-				sNode = sNode.next.next;
-				c2 = c2.next;
+				else if(c1 == null) {
+					sNode.next = new ListNode(c2.val);
+					c2 = c2.next;
+				}
 			}
-
-			// sNode = sNode.next;
-
+				
+			if(c1 != null) {
+				sNode.next = new ListNode(c1.val);
+				sNode = sNode.next;
+				c1 = c1.next;
+			}
 		}
 
 		return newList.next;
@@ -79,25 +91,18 @@ public class Solution {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		// [1,2,4]
-		// [1,3,4]
-		// newList -> 1 -> 1 -> 2 -> 3 -> 4 -> 4
 		ListNode l1 = new ListNode(1);
 		l1.next = new ListNode(2);
 		l1.next.next = new ListNode(4);
+		
 		ListNode l2 = new ListNode(1);
 		l2.next = new ListNode(3);
 		l2.next.next = new ListNode(4);
 
-		// [1]
-		// []
-		// newList -> 1
 		ListNode l3 = new ListNode(1);
+		
 		ListNode l4 = null;
 
-		// [-10,-10,-9,-4,1,6,6]
-		// [-7]
-		// newList -> 10 -> -10 -> -9 -> -7 -> -4 -> 1 -> 6 -> 6
 		ListNode l5 = new ListNode(-10);
 		l5.next = new ListNode(-10);
 		l5.next.next = new ListNode(-9);
@@ -105,40 +110,66 @@ public class Solution {
 		l5.next.next.next.next = new ListNode(1);
 		l5.next.next.next.next.next = new ListNode(6);
 		l5.next.next.next.next.next.next = new ListNode(6);
+		
 		ListNode l6 = new ListNode(-7);
+		
+		ListNode l7 = new ListNode(1);
+		
+		ListNode l8 = new ListNode(2);
 
-		// testing list output
-		for (ListNode cursor = l1; cursor != null; cursor = cursor.next) {
+		ListNode l9 = new ListNode(-9);
+		l9.next = new ListNode(3);
+		ListNode l10 = new ListNode(5);
+		l10.next = new ListNode(7);
+	
+		for (ListNode cursor = l9; cursor != null; cursor = cursor.next) {
 			System.out.print(cursor.val + " ");
 		}
 
 		System.out.println();
 		
-		// testing list output
-		for (ListNode cursor = l2; cursor != null; cursor = cursor.next) {
+		for (ListNode cursor = l10; cursor != null; cursor = cursor.next) {
 			System.out.print(cursor.val + " ");
 		}
 
 		System.out.println();
 
 		Solution retList = new Solution();
-		ListNode ans = retList.mergeTwoLists(l1, l2);
-		ListNode ans2 = retList.mergeTwoLists(l3, l4);
-	    ListNode ans3 = retList.mergeTwoLists(l5, l6);
+		//ListNode ans1 = retList.mergeTwoLists(l1, l2);
+		//ListNode ans2 = retList.mergeTwoLists(l3, l4);
+	  // ListNode ans3 = retList.mergeTwoLists(l5, l6);
+	  // ListNode ans4 = retList.mergeTwoLists(l7, l8);
+	   ListNode ans5 = retList.mergeTwoLists(l9, l10);
 
-		// testing ans output
-		for (ListNode cursor = ans; cursor != null; cursor = cursor.next) {
-			System.out.print(cursor.val + " ");
-		}
+		//for (ListNode cursor = ans1; cursor != null; cursor = cursor.next) {
+			//System.out.print(cursor.val + " ");
+		//}
+		
 		System.out.println();
-		for (ListNode cursor = ans2; cursor != null; cursor = cursor.next) {
-			System.out.print(cursor.val + " ");
-		}
+		
+		//for (ListNode cursor = ans2; cursor != null; cursor = cursor.next) {
+		//	System.out.print(cursor.val + " ");
+		//}
+		
 		System.out.println();
-		for (ListNode cursor = ans3; cursor != null; cursor = cursor.next) {
-			System.out.print(cursor.val + " ");
-		}
+		
+		//for (ListNode cursor = ans3; cursor != null; cursor = cursor.next) {
+			//System.out.print(cursor.val + " ");
+		//}
+		
+		System.out.println();
 
+		
+		//for (ListNode cursor = ans4; cursor != null; cursor = cursor.next) {
+			//System.out.print(cursor.val + " ");
+		//}
+		
+		System.out.println();
+
+
+		for (ListNode cursor = ans5; cursor != null; cursor = cursor.next) {
+			System.out.print(cursor.val + " ");
+		}
 	}
 
 }

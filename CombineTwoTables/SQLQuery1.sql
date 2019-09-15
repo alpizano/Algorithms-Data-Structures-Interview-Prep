8a5cc8a1e671462e80b238dd47c3cc76b8ce8c65
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-09-15 06:39:47 -0500

    starting HappyNumber problem

--- CombineTwoTables/SQLQuery1.sql
+++ CombineTwoTables/SQLQuery1.sql
@@ -1,5 +1,5 @@
 /* Write your T-SQL query statement below */
-SELECT p.PersonId, p.FirstName, p.LastName, a.City, a.State
+SELECT p.FirstName, p.LastName, a.City, a.State
 FROM Person p
-INNER JOIN Address a
+LEFT JOIN Address a
 ON p.PersonId = a.PersonId
---
+++ HappyNumber/src/Solution.java
@@ -0,0 +1,13 @@
+
+public class Solution {
+	
+	public boolean isHappy(int n) {
+        
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

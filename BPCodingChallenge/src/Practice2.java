ec85978650350a597afa92413979d266458c11e3
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-09-18 18:29:19 -0500

    solved binary to decimal conversion both iteratively and recursively

--- BPCodingChallenge/src/Practice2.java
+++ BPCodingChallenge/src/Practice2.java
@@ -1,24 +1,37 @@
 
 public class Practice2 {
+	int k = 0;
+	static int sum = 0;
+	static int recur_sum=0;
 	
-	public static int binToDec(String binary) {
+	
+	  public static int binToDec(String binary) {
+	  
+	  int num = Integer.parseInt(binary); // convert string to int 
+	  int i = 0;
+	  
+	  while(num > 0) { // take last digit if(num % 10 == 1) { 
+	  sum = sum + (num%10)*(int)Math.pow(2, i);
+	   } 
+	   num= num / 10; // increment i++; } 
+	   return sum; 
+	   }
+	 
+	
+	public static int binToDec_recur(String binary, int k) {
+		int num = Integer.parseInt(binary);
 		
-		int num = Integer.parseInt(binary); // convert string to int
-		int sum = 0;
-		int i = 0;
-		
-		while(num > 0) {
-			// take last digit
-			if(num % 10 == 1) { 
-				sum = sum + (num%10)*(int)Math.pow(2, i);
-			}
-
-			
-			num= num / 10; // increment
-			i++;
+		if(num <= 0 ) {
+			return recur_sum;
 		}
-		
-		return sum;
+		else {
+			if(num % 10 == 1) {
+				recur_sum = recur_sum + (num%10)*(int)Math.pow(2, k);				
+			}
+			num = num/10;
+			k++;
+			return binToDec_recur(String.valueOf(num),k);
+		}
 	}
 
 	public static void main(String[] args) {
@@ -29,8 +42,11 @@ public class Practice2 {
 		
 		String num1 = "110011"; // 110011 = 51
 		String num2 = "1010"; // 1010 = 10
+		String num3 = "10000"; // 16
 
 	    System.out.println(Practice2.binToDec(num2));
+	    
+	    System.out.println(Practice2.binToDec_recur(num3, 0));
 
 	}
 

81d22e03800f1bfb11c23080e99458404ebaf7d4
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-09-18 20:46:03 -0500

    Implemented custom Insertion sort algorithm

--- InsertionSort/src/Solution.java
+++ InsertionSort/src/Solution.java
@@ -1,9 +1,34 @@
+// Insertion Sort
 
 public class Solution {
+
+	public int[] sort(int[] num) {
+
+		for (int i = 1; i <= num.length - 1; i++) {
+			int temp_value = num[i];
+			int gap = i;
+
+			while (gap >= 1 && temp_value < num[gap - 1]) {
+				num[gap] = num[gap - 1];
+				gap--;
+
+			}
+			num[gap] = temp_value;
+		}
+
+		return num;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+		int[] num_arr = new int[] { 4, 2, 7, 1, 3 };
 
+		Solution test1 = new Solution();
+		int[] ret_arr = test1.sort(num_arr);
+
+		for (int i : ret_arr) {
+			System.out.print(i);
+		}
+	}
 }

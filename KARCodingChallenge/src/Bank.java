f1c4b4ad9fd32e66cfc6708a201b4f50f65d06a4
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-09-18 23:15:55 -0500

    finished transfer method

--- KARCodingChallenge/src/Bank.java
+++ KARCodingChallenge/src/Bank.java
@@ -14,15 +14,17 @@ public class Bank {
 		bankAccounts = new ArrayList<>();
 	}
 
-	// need to fix this constructor
-	public Bank(String name, ArrayList bankAccounts) {
-		this.name = name;
-		this.bankAccounts = bankAccounts;
-	}
-
 	public void addAccount(String owner, int balance) {
 		Account new_acct = new Account(owner, balance);
 		bankAccounts.add(new_acct);
+	}
+	
+	public void deposit(String accountName, int num) {
+		for (Account a : bankAccounts) {
+			if (a.owner.equals(accountName)) {
+				a.balance = a.balance + num;
+			}
+		}
 	}
 
 	public void withdraw(String accountName, int num) {
@@ -39,6 +41,22 @@ public class Bank {
 			}
 		}
 	}
+	
+	public void transfer(String fromAccountName, String toAccountName, int num) {
+		for (Account a : bankAccounts) {
+			if (a.owner.equals(fromAccountName)) {
+				if (a.balance - num >= 0) 
+				{
+					a.balance = a.balance - num;
+				} 
+			}
+			if(a.owner.equals(toAccountName)) {
+				a.balance = a.balance + num;
+			}
+		}
+	}
+	
+	
 
 	class Account {
 		String owner;
@@ -53,60 +71,51 @@ public class Bank {
 			this.owner = owner;
 			this.balance = balance;
 		}
-
-		/*
-		 * public void deposit(int num) { balance = balance + num; }
-		 * 
-		 * public void withdraw(int num) { if(num > 1000) {
-		 * System.out.println("Withdrawal limit is 1000 dollars!"); } if(num >= balance)
-		 * { balance = balance -num; } else { System.out.
-		 * println("You do not have sufficient balance to withdraw this amount!"); } }
-		 * 
-		 * public void transfer(int num) {
-		 * 
-		 * }
-		 */
-
 	}
 
 	// inherits from Account superclass
 	class Checking extends Account {
-
 	}
 
 	// inherits from Account superclass
 	class Savings extends Account {
-
 	}
 
 	class Individual extends Checking {
-
 	}
 
 	class MoneyMarket extends Checking {
-
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-		// invoke a deposit test
+		// invoke a constructor deposit test
 		Bank sample_bank = new Bank("Javier's Bank");
 		sample_bank.addAccount("Javier's Account", 500);
-		sample_bank.addAccount("Matt Massoms Account", 666);
-		sample_bank.addAccount("Preston's account", 9999);
+		sample_bank.addAccount("Matt Massom's Account", 666);
+		sample_bank.addAccount("Preston's Account", 9999);
 
 		// testing deposit values
 		for (Account a : sample_bank.bankAccounts) {
 			System.out.println("The owner of this account is: " + a.owner + " and the balance is: " + a.balance);
 		}
-
+		
+		System.out.println();
+		
+		// deposit using method
+		sample_bank.deposit("Javier's Account", 500);
+		
+		// testing deposit values
+		for (Account a : sample_bank.bankAccounts) {
+			System.out.println("The owner of this account is: " + a.owner + " and the balance is: " + a.balance);
+		}
+		
 		System.out.println();
 
 		// invoke withdrawal
 		sample_bank.withdraw("Javier's Account", 300);
-		sample_bank.withdraw("Matt Massoms Account", 600);
-		sample_bank.withdraw("Preston's account", 10000);
+		sample_bank.withdraw("Matt Massom's Account", 600);
+		sample_bank.withdraw("Preston's Account", 10000);
 
 		System.out.println();
 
@@ -114,6 +123,17 @@ public class Bank {
 		for (Account a : sample_bank.bankAccounts) {
 			System.out.println("The owner of this account is: " + a.owner + " and the balance is: " + a.balance);
 		}
+		
+		System.out.println();
+		
+		// invoking transfer
+		sample_bank.transfer("Matt Massom's Account", "Preston's Account", 50);
+		
+		// testing transfer values
+				for (Account a : sample_bank.bankAccounts) {
+					System.out.println("The owner of this account is: " + a.owner + " and the balance is: " + a.balance);
+				}
+				
 	}
 }
 

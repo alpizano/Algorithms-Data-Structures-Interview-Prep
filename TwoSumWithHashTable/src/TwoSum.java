c4ab2921dadffba97e6f64020d46beb73b0e5e0e
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-09-21 11:51:51 -0500

    updated TwoSum with hash table

--- TwoSumWithHashTable/src/TwoSum.java
+++ TwoSumWithHashTable/src/TwoSum.java
@@ -1,44 +1,32 @@
 import java.util.HashMap; // import the HashMap class
+import java.util.Map;
 
 public class TwoSum {
 	
 	//public static void twoSum(int[] nums, int target) {
 	public static int[] twoSum(int[] nums, int target) {
 		
-		HashMap<Integer,Integer> hashSum = new HashMap<Integer,Integer>();
-		int[] ans = new int[2];
-		
-		// iterate thru array and put values in hash map
-		for (int i=0; i<nums.length; i++) {
-			hashSum.put(nums[i],i); // value of array is key, index is value
-		}
-		
-		for(Integer i : hashSum.keySet()) {
-			Integer comp = target - i;		
-			 
-			Integer compIndex = hashSum.get(comp);
-			//System.out.println("Test null: " + compIndex);
-			if(hashSum.get(comp) != null) {
-				ans[0] = hashSum.get(i);
-				ans[1] = hashSum.get(comp);	
-			}
-			System.out.println("value is: " + i + " at index: " + hashSum.get(i) + ", complement: " + comp);
-		}
-		
-		//System.out.println("--------------------------");
-		//for(int i : nums) {
-			//System.out.println(i);
-		//}
-		return ans;
+		  Map<Integer, Integer> map = new HashMap<>();
+		    for (int i = 0; i < nums.length; i++) {
+		        map.put(nums[i], i);
+		    }
+		    for (int i = 0; i < nums.length; i++) {
+		        int complement = target - nums[i];
+		        if (map.containsKey(complement) && map.get(complement) != i) {
+		            return new int[] { i, map.get(complement) };
+		        }
+		    }
+		    throw new IllegalArgumentException("No two sum solution");
 	}
 
 
 	public static void main(String args[]) {
 
-    int[] newArr = new int[] {5, 23, 1, 99, 7, 69};
-    int[] retArr = new int[6];
+    //int[] newArr = new int[] {5, 23, 1, 99, 7, 69};
+    int[] newArr = new int[] {3,2,4};
+   
     
-    retArr = TwoSum.twoSum(newArr,30);
+    int[] retArr = TwoSum.twoSum(newArr,6);
     
     for(int i : retArr) {
     	System.out.println(i);

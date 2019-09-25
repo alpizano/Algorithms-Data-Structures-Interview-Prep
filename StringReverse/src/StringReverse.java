4a8def19e46bee110273c1bbbb6fec88cb6ee246
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-09-25 13:32:27 -0500

    finished reversestring leetcode

--- StringReverse/src/StringReverse.java
+++ StringReverse/src/StringReverse.java
@@ -3,19 +3,47 @@ public class StringReverse {
 	 public void reverseString(char[] s) {
 	        
 		 int j = s.length-1;
-		
-		 for(int i =0; i <= s.length -1; i++ ) {
-			 char temp = s[i] ;
-			 s[i] = s[j];
-			 s[j] = temp;
+		 
+		 if(s.length == 0 ) {
+			 
 		 }
+
+				 // even
+		 else if( s.length %2 == 0 && s.length != 0) {
+				 for(int i =0; i <= (s.length-1)/2; i++ ) {
+				 char temp = s[i] ;
+				 s[i] = s[j];
+				 s[j] = temp;
+				 j--;
+				 }
+			 }
+			 else {
+				 for(int i =0; i < (s.length-1)/2; i++ ) {
+				 char temp = s[i] ;
+				 s[i] = s[j];
+				 s[j] = temp;
+				 j--;
+				 }
+			 }
+		 
 	    }
 	 
 	 public static void main(String[] args) {
 		 char[] test = {'H', 'e', 'l', 'l' ,'o' };
+		 char[] test2 = {};
 		 
-		 Solution
+		 System.out.println(test2.length);
 		 
+		 StringReverse blah =  new StringReverse();
+			System.out.println(test);
+
+		blah.reverseString(test);
+		blah.reverseString(test2);
+		
+		System.out.println(test);
+		 
+		int ans = 5/2;
+		System.out.println(ans);
 	 }
 
 }

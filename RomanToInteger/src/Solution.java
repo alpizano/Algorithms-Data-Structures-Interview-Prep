d67cced0d1fafbb3fd8b8d52ebb84c0e63765841
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-09-26 08:25:44 -0500

    finished RomanToInteger leet code problem

--- RomanToInteger/src/Solution.java
+++ RomanToInteger/src/Solution.java
@@ -1,8 +1,72 @@
 
 public class Solution {
 
+	public static int convertDigit(String s) {
+		if (s.equals("I")) {
+			return 1;
+		} else if (s.equals("V")) {
+			return 5;
+		} else if (s.equals("X")) {
+			return 10;
+		} else if (s.equals("L")) {
+			return 50;
+		} else if (s.equals("C")) {
+			return 100;
+		} else if (s.equals("D")) {
+			return 500;
+		} else {
+			return 1000;
+		}
+	}
+
+	public int romanToInt(String s) {
+		String output = "";
+		int sum = 0;
+
+		for (int i = 0; i <= s.length() - 1; i++) {
+			if (i < s.length() - 1 && Solution.convertDigit(String.valueOf(s.charAt(i))) == 1
+					&& (Solution.convertDigit(String.valueOf(s.charAt(i + 1))) == 5
+							|| Solution.convertDigit(String.valueOf(s.charAt(i + 1))) == 10)) {
+				sum = sum + (Solution.convertDigit(String.valueOf(s.charAt(i + 1)))
+						- Solution.convertDigit(String.valueOf(s.charAt(i))));
+				i++;
+			} else if (i < s.length() - 1 && Solution.convertDigit(String.valueOf(s.charAt(i))) == 10
+					&& (Solution.convertDigit(String.valueOf(s.charAt(i + 1))) == 50
+							|| Solution.convertDigit(String.valueOf(s.charAt(i + 1))) == 100)) {
+				sum = sum + (Solution.convertDigit(String.valueOf(s.charAt(i + 1)))
+						- Solution.convertDigit(String.valueOf(s.charAt(i))));
+				i++;
+			} else if (i < s.length() - 1 && Solution.convertDigit(String.valueOf(s.charAt(i))) == 100
+					&& (Solution.convertDigit(String.valueOf(s.charAt(i + 1))) == 500
+							|| Solution.convertDigit(String.valueOf(s.charAt(i + 1))) == 1000)) {
+				sum = sum + (Solution.convertDigit(String.valueOf(s.charAt(i + 1)))
+						- Solution.convertDigit(String.valueOf(s.charAt(i))));
+				i++;
+			} else {
+				sum = sum + Solution.convertDigit(String.valueOf(s.charAt(i)));
+			}
+		}
+		return sum;
+
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+		String test0 = "III";
+		String test1 = "IV";
+		String test2 = "IX";
+		String test3 = "LVIII";
+		String test4 = "MCMXCIV";
+
+		Solution sol = new Solution();
+		System.out.println(sol.romanToInt(test0));
+		System.out.println(sol.romanToInt(test1));
+		System.out.println(sol.romanToInt(test2));
+		System.out.println(sol.romanToInt(test3));
+		System.out.println(sol.romanToInt(test4));
+		
 
 	}
 

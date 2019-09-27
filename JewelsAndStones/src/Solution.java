7b7129d538b8889c8b62b98a43c5c55bd14e4ea9
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-09-26 19:22:16 -0500

    finished jewels and stoned leetcode problem

--- JewelsAndStones/src/Solution.java
+++ JewelsAndStones/src/Solution.java
@@ -1,39 +1,38 @@
 import java.util.HashSet;
 
 public class Solution {
-	public int numJewelsInStones(String J,String S) {
-HashSet<String> set = new HashSet<>();
+	public int numJewelsInStones(String J, String S) {
+		HashSet<String> set = new HashSet<>();
 
 		int count = 0;
-		
-		for(int i=0; i<=J.length()-1; i++) {
+
+		for (int i = 0; i <= J.length() - 1; i++) {
 			set.add(String.valueOf(J.charAt(i)));
 		}
-		
-		for(int i=0; i<=S.length()-1; i++) {
-			if(set.contains(String.valueOf(S.charAt(i)))) {
-				count ++;
+
+		for (int i = 0; i <= S.length() - 1; i++) {
+			if (set.contains(String.valueOf(S.charAt(i)))) {
+				count++;
 			}
 		}
 
 		return count;
-		
+
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		String J = "aA";
 		String S = "aAAbbbb";
 		String J2 = "z";
 		String S2 = "ZZ";
-		
+
 		Solution test = new Solution();
-		
+
 		System.out.println(test.numJewelsInStones(J, S));
 		System.out.println(test.numJewelsInStones(J2, S2));
 
-		
 	}
 
 }

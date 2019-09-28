2deb0c3732dbdde5dfa13ac7dcbca5bec276c01b
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-09-28 16:39:51 -0500

    formatted the code prettier

--- SubdomainVisitCount/src/Solution.java
+++ SubdomainVisitCount/src/Solution.java
@@ -41,31 +41,35 @@ public class Solution {
 						subdomain_compare_padded[0] = " ";
 						subdomain_compare_padded[1] = subdomain_compare[0];
 						subdomain_compare_padded[2] = subdomain_compare[1];
-					} else
-					{
+					} else {
 						subdomain_compare_padded = subdomain_compare;
 					}
 
 					if (subdomain_control_padded[2].equals(subdomain_compare_padded[2])) {
 						count_arr[2] = count_arr[2] + domain_count_compare;
-						}
-					if (subdomain_control_padded[2].equals(subdomain_compare_padded[2]) && subdomain_control_padded[1].equals(subdomain_compare_padded[1])) {
-							count_arr[1] = count_arr[1] + domain_count_compare;
-						}
-					if (subdomain_control_padded[2].equals(subdomain_compare_padded[2]) && subdomain_control_padded[1].equals(subdomain_compare_padded[1]) && subdomain_control_padded[0].equals(subdomain_compare_padded[0])) {
-							count_arr[0] = count_arr[0] + domain_count_compare;
-						}
 					}
-				
+					if (subdomain_control_padded[2].equals(subdomain_compare_padded[2])
+							&& subdomain_control_padded[1].equals(subdomain_compare_padded[1])) {
+						count_arr[1] = count_arr[1] + domain_count_compare;
+					}
+					if (subdomain_control_padded[2].equals(subdomain_compare_padded[2])
+							&& subdomain_control_padded[1].equals(subdomain_compare_padded[1])
+							&& subdomain_control_padded[0].equals(subdomain_compare_padded[0])) {
+						count_arr[0] = count_arr[0] + domain_count_compare;
+					}
+				}
+
 			} // end inner loop
 
 			set.add(String.valueOf(count_arr[2]) + " " + subdomain_control_padded[2]);
-			set.add(String.valueOf(count_arr[1]) + " " + subdomain_control_padded[1] + "." + subdomain_control_padded[2]);
-			
+			set.add(String.valueOf(count_arr[1]) + " " + subdomain_control_padded[1] + "."
+					+ subdomain_control_padded[2]);
+
 			if (!subdomain_control_padded[0].equals(" ")) {
-				set.add(String.valueOf(count_arr[0]) + " " + subdomain_control_padded[0] + "." + subdomain_control_padded[1] + "." + subdomain_control_padded[2]);
+				set.add(String.valueOf(count_arr[0]) + " " + subdomain_control_padded[0] + "."
+						+ subdomain_control_padded[1] + "." + subdomain_control_padded[2]);
 			}
-	}  // end outer loop
+		} // end outer loop
 
 		for (String s : set) {
 			list.add(s);
@@ -75,18 +79,15 @@ public class Solution {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		String[] str_arr1 = new String[] {"9001 discuss.leetcode.com"};
-		String[] str_arr2 = new String[] {"900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"};
-		
+
+		String[] str_arr1 = new String[] { "9001 discuss.leetcode.com" };
+		String[] str_arr2 = new String[] { "900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org" };
+
 		Solution test = new Solution();
-		
+
 		test.subdomaininVisists(str_arr2);
-	
-		
+
 		System.out.println(test.subdomaininVisists(str_arr2));
-		
-		
-		
-}
+
+	}
 }

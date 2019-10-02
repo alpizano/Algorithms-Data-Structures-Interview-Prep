892470d8bd0ac3384c7c4669cc4a959fd0ef97a1
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-10-02 03:05:32 -0500

    finished implement str str

--- ImplementStrStr/src/Solution.java
+++ ImplementStrStr/src/Solution.java
@@ -3,26 +3,32 @@ import java.util.HashMap;
 public class Solution {
 	
 	public int strStr(String haystack, String needle) {
-		
-		HashMap<String, Integer> map = new HashMap<>();
 		int index = 0;
 		
-		for(int i=0; i<=haystack.length()-1; i++) {
-			map.put(String.valueOf(haystack.charAt(i)), i);
-		}
-		
 		for(int i=0; i<=needle.length()-1; i++) {
-		if(map.containsKey(String.valueOf(needle.charAt(i)))) {
-			index = map.get(String.valueOf(needle.charAt(i)));
+			if(haystack.indexOf(needle) != -1) {
+			index = haystack.indexOf(needle);
 		}
-		}
-		
+			else {
+				return -1;
+			}					
+	}
 		return index;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		String haystack = "hello";
+		String needle = "ll";
+		
+		Solution test = new Solution();
+		
+		
+		//String haystack = "aaaaa";
+		//String needle  = "bba";
+		
+		System.out.println(test.strStr(haystack, needle));
 	}
 
 }

6041f305d2a09a0b1186d05cea02c0da3f6c70a3
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-10-08 00:06:12 -0500

    added MoveZeros leetcode problem and moved codingchallenges to sepereate repository

--- MoveZeros/src/Solution.java
+++ MoveZeros/src/Solution.java
@@ -1,5 +1,8 @@
 
-public class PartTwoProblemOne {
+public class Solution {
+	public void moveZeroes(int[] nums) {
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

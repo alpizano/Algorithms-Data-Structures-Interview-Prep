38607ceef596b3fbba07f341bb203c8d07780568
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-10-11 23:11:36 -0500

    added AddBinary leetcode problem

---
+++ AddBinary/src/Solution.java
@@ -0,0 +1,20 @@
+
+public class Solution {
+	  public String addBinary(String a, String b) {
+	        
+	    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "11";
+		String b = "1";
+		
+		String a1 = "1010";
+		String b1 = "1011";
+		
+		Solution test = new Solution();
+		
+		test.addBinary(a, b);
+	}
+
+}

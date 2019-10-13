893c1dc6e64997e71c3d9a8465fdb78f35f7c9fa
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-10-12 22:13:49 -0500

    Started LongestCommonPrefix

---
+++ LongestCommonPrefix/src/Solution.java
@@ -0,0 +1,19 @@
+
+public class Solution {
+	public String longestCommonPrefix(String[] strs) {
+		
+		
+		return strs;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] strs = new String[] {"flower", "flow", "flight"};
+		
+		Solution test = new Solution();
+		
+		test.longestCommonPrefix(strs);
+		
+	}
+
+}

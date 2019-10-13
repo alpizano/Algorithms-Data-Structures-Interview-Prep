e1b9c550b41f2a63c8e748a9fa06737f92c184c7
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-10-12 23:27:06 -0500

    writing nested for loop for longest common prefix checker

--- LongestCommonPrefix/src/Solution.java
+++ LongestCommonPrefix/src/Solution.java
@@ -1,9 +1,43 @@
+import java.util.Map;
+import java.util.HashMap;
 
 public class Solution {
 	public String longestCommonPrefix(String[] strs) {
+		HashMap<String,Integer> map = new HashMap<>();
+		StringBuilder sb_control = new StringBuilder();
+		StringBuilder sb_lcp = new StringBuilder();
+		String str_lcp = "";
+		boolean containsVal = true;
 		
+		for(int i=0; i<=strs.length-1; i++) {
+			map.put(strs[i], i);
+		}
 		
-		return strs;
+		int x = 0;
+		for(int y=0; y<=strs.length-1; y++) {
+			sb_control.append(strs[0].charAt(x));
+			for(int j=y+1; j<=strs.length-1; j++) {
+				if(!strs[j].contains(sb_control.toString())) {
+					containsVal = false;
+				}
+				if(j == strs.length-1 && containsVal == true) {
+					str_lcp = sb_control.toString();
+				}
+				else {
+					break;
+				}
+				x++;
+			}
+		}
+		
+		if(sb_control.length() == 0) {
+			return "";
+		}
+		else {
+
+		
+		return str_lcp;
+		}
 	}
 
 	public static void main(String[] args) {
@@ -12,7 +46,9 @@ public class Solution {
 		
 		Solution test = new Solution();
 		
-		test.longestCommonPrefix(strs);
+		System.out.println(test.longestCommonPrefix(strs));
+		
+	
 		
 	}
 
